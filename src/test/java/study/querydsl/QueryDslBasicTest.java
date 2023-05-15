3165a65035a4049b92255bd2043080f6e80d5f2f
ohy1023 <zvyg1023@naver.com>
2023-05-15 14:34:58 +0900

    시작 - JPQL vs Querydsl

---
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -0,0 +1,74 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QueryDslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    @DisplayName("JPQL로 조회")
+    public void startJPQL() throws Exception{
+        // member1을 찾아라.
+        Member findMember = em.createQuery("select m from Member m where m.userName = :userName", Member.class)
+                .setParameter("userName", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUserName()).isEqualTo("member1");
+    }
+    
+    @Test
+    @DisplayName("QueryDsl로 조회")
+    public void startQueryDsl() throws Exception {
+    
+        // given
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        // when
+        Member findMember = queryFactory.select(m)
+                .from(m)
+                .where(m.userName.eq("member1"))
+                .fetchOne();
+
+        // then
+        assertThat(findMember.getUserName()).isEqualTo("member1");
+        
+    }
+        
+}
